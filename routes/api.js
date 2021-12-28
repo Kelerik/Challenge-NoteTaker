d52ea76f2fbf053dc31af33b40e705dba567cf4b
Kelerik <vincent.ng39@gmail.com>
2021-12-28 14:29:58 -0500

    save note working

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 const db = require("../db/db");
+const saveDb = require("../utils/saveDb");
 
 router.get("/notes", (req, res) => {
    res.json(db);
+});
+
+router.post("/notes", (req, res) => {
+   saveDb(req.body)
+      .then((output) => {
+         res.send(output);
+      })
+      .catch((err) => {
+         res.send(err);
+      });
 });
 
 module.exports = router;
---
+++ utils/saveDb.js
@@ -0,0 +1,37 @@
+// dependencies
+const fs = require("fs");
+const path = require("path");
+const { nanoid } = require("nanoid");
+// data
+let db = require("../db/db");
+
+const saveDb = (noteObj) => {
+   return new Promise((resolve, reject) => {
+      // validate data
+      if (!noteObj.title) {
+         reject("Error: 'title' property missing");
+         return;
+      } else if (!noteObj.text) {
+         reject("Error: 'text' property missing");
+         return;
+      }
+      // generate new uuid
+      noteObj.id = nanoid();
+      // add to db
+      db.push(noteObj);
+      // write file
+      fs.writeFile(
+         path.join(__dirname, "../db/db.json"),
+         JSON.stringify(db, null, 2),
+         (err) => {
+            if (err) {
+               reject(err);
+               return;
+            }
+            resolve("Successully added new note to database.");
+         }
+      );
+   });
+};
+
+module.exports = saveDb;
